b1ed12e225d4ec7eb6144622916ab3e868ff1dca
Lenne Hendrickx <lenne.h@sparkcentral.com>
2021-09-28 22:01:49 +0200

    Add video store exercise

---
+++ src/test/java/com/cleancode/videostore/CustomerTest.java
@@ -0,0 +1,28 @@
+package com.cleancode.videostore;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.cleancode.videostore.Movie.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CustomerTest {
+
+    @Test
+    public void statementTest() {
+        var customer = new Customer("John Doe");
+        customer.addRental(new Rental(new Movie("Star Wars", NEW_RELEASE), 6));
+        customer.addRental(new Rental(new Movie("Sofia", CHILDRENS), 7));
+        customer.addRental(new Rental(new Movie("Inception", REGULAR), 5));
+        
+        var expected = "Rental Record for John Doe\n"
+                + "	Star Wars	18.0\n"
+                + "	Sofia	7.5\n"
+                + "	Inception	6.5\n"
+                + "Amount owed is 32.0\n"
+                + "You earned 4 frequent renter points";
+        
+        assertThat(customer.statement()).isEqualTo(expected);
+    }
+}
